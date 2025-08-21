1a0399676ff98e336f44dbd07feb3662489c6bc3
Taegyun Kim <kimtg2004@gmail.com>
2025-08-21 16:53:21 +0900

    [feat] OUTER, CROSS, SELF JOIN 추가

--- Join.sql
+++ Join.sql
@@ -1,3 +1,4 @@
+USE DoItSQL
 /*
 1. INNER JOIN
 --PrimaryForm
@@ -51,6 +52,7 @@ INNER JOIN [TABLE 3] ON [TABLE 2.ROW] = [TABLE 3.ROW]
 WHERE [CONDITION]
 */
 
+/*
 SELECT
 	a.industry,
 	c.symbol,
@@ -62,3 +64,100 @@ FROM industry_group AS a
 	INNER JOIN nasdaq_company AS c ON b.symbol = c.symbol 
 WHERE a.industry = N'ÀÚµ¿Â÷'
 ORDER BY symbol
+*/
+
+/*
+4. OUTER JOIN
+--PrimaryForm
+
+SELECT [ROW]
+FROM [TABLE 1]
+<LEFT, RIGHT, FULL> OUTER JOIN [TABLE 2] ON [TABLE 1.ROW] = [TABLE 2]
+WHERE [CONDITION]
+*/
+
+/*
+5. LEFT OUTER JOIN
+SELECT
+	a.symbol AS a_symbol,
+	b.symbol AS b_symbol
+FROM nasdaq_company AS a
+	LEFT OUTER JOIN industry_group_symbol AS b ON a.symbol = b.symbol
+WHERE b.symbol IS NULL
+*/
+
+/*
+6. RIGHT JOIN
+SELECT
+	a.symbol AS a_symbol,
+	b.symbol AS b_symbol
+FROM industry_group_symbol AS a
+	RIGHT OUTER JOIN nasdaq_company AS b ON a.symbol = b.symbol
+WHERE a.symbol IS NULL
+*/
+
+/*
+7. FULL OUTER JOIN
+SELECT
+	a.symbol AS a_symbol,
+	b.symbol AS b_symbol
+FROM industry_group_symbol AS a
+	FULL OUTER JOIN nasdaq_company AS b ON a.symbol = b.symbol
+*/
+
+/*
+SELECT
+	a.symbol AS a_symbol,
+	b.symbol AS b_symbol
+FROM nasdaq_company AS a
+	FULL OUTER JOIN industry_group_symbol AS b ON a.symbol = b.symbol
+WHERE a.symbol IS NULL
+	OR b.symbol IS NULL
+*/
+
+/*
+8. CROSS JOIN
+--PrimaryForm
+
+SELECT [ROW
+FROM [TABLE 1]
+	CROSS JOIN [TABLE 2]
+WHERE [CONDITION]
+*/
+
+/*
+--MakeSampleData
+
+CREATE TABLE doit_cross1(num INT);
+CREATE TABLE doit_cross2(name NVARCHAR(10));
+INSERT INTO doit_cross1 VALUES (1),(2),(3)
+INSERT INTO doit_cross2 VALUES ('Do'),('It'),('SQL')
+*/
+
+/*
+SELECT
+	a.num, b.name
+FROM doit_cross1 AS a
+	CROSS JOIN doit_cross2 AS b
+WHERE a.num =1
+*/
+
+/*
+--DeleteSampleData
+DROP TABLE doit_cross1
+DROP TABLE doit_cross2
+*/
+
+
+--9. SELF JOIN
+SELECT
+	a.[date] AS a_date,
+	a.[close] AS a_close,
+	b.[date] AS b_date,
+	b.[close] AS b_close,
+	b.[close] - a.[close] AS diff_close
+FROM stock AS a
+	LEFT OUTER JOIN stock AS b ON a.date = dateadd(day, -1, b.date)
+WHERE a.symbol = 'MSFT' AND b.symbol = 'MSFT'
+	AND a.date >= '2021-10-01' AND a.date < '2021-11-01'
+	AND b.date >= '2021-10-01' AND b.date < '2021-11-01'
